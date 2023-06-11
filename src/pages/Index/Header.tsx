84e0af4298bdaa674ea6ea57a1a7babbb8fdb958
Zionified <zixuanzheng@Zixuans-MacBook-Pro.local>
2023-06-11 18:39:29 -0400

    modified intro

--- src/pages/Index/Header.tsx
+++ src/pages/Index/Header.tsx
@@ -111,7 +111,7 @@ const Header = () => {
                     <div>Carnegie Mellon University</div>
                 </Subtitle>
                 <ShortIntro>
-                    Machine Learning enthusiast, Web Developer, Golfer
+                    Machine Learning Enthusiast🤖️, Web Developer💻, Golf Athlete🏌️
                 </ShortIntro>
                 <Nav></Nav>
             </div>
